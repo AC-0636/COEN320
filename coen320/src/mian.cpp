c683350c1c41ccbfbc6502d4208bbfcb390e79ba
AC-0636 <33075704+AC-0636@users.noreply.github.com>
2019-11-09 16:00:00 -0500

    Update mian.cpp

--- coen320/src/mian.cpp
+++ coen320/src/mian.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <math.h>
+#include <chrono>
+#include <thread>
 #include "TestCase.h"
 #include "Airplane.h"
 
+#define update_view 5
 
 using namespace std;
 
-int main()
+//thread 1 for plan view display, update every 5s
+//thread 2 for log file output every 60s
+//thread 3 for safety alarm
+//thread 4 for ATC command
+
+
+void timer_start(function<void(void)> func, unsigned int interval)
 {
-	Airplane P1(1,2,3,4,5,7,7,8);
-	cout << P1.getData('X')<<endl;
+      while (true)
+      {
+          func();
+          this_thread::sleep_for(chrono::milliseconds(interval));
+      }
+}
+
+void print_plan()
+{
+	cout<<"update view every 5s" <<endl;
+}
+
+void history_log()
+{
 
 }
 
-
+int main()
+{
+	timer_start(print_plan,5000);
+	while (true);
+}
