8732a80388948c9ecafe283b1d718c12ea623328
Oussama <oussamus_84@hotmail.com>
2019-11-11 13:09:29 -0500

    Collisions detection
    The program is able to detect if the minimal distance between two plans is respected or no

--- coen320/src/Airplane.h
+++ coen320/src/Airplane.h
@@ -1,5 +1,8 @@
-
-class Airplane{
+#include <time.h>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+class Airplane {
 
 public:
 	int ID;
@@ -22,10 +25,10 @@ public:
 		Z = z;
 		Entry_timE = entry_time;
 		//assume test for position for 5s in the future
-		Next_X = 5*(speed_x*x);
-		Next_Y = 5*(speed_y*y);
-		Next_Z = 5*(speed_z*z);
-		DisplacemenT = sqrt(pow((Next_X-X),2)+pow((Next_Y-Y),2)+pow((Next_Z-Z),2));
+		Next_X = 5 * (speed_x * x);
+		Next_Y = 5 * (speed_y * y);
+		Next_Z = 5 * (speed_z * z);
+		DisplacemenT = sqrt(pow((Next_X - X), 2) + pow((Next_Y - Y), 2) + pow((Next_Z - Z), 2));
 
 	}
 
@@ -43,11 +46,78 @@ public:
 		{
 			return Next_Z;
 		}
-		else if (item == 'D' )
+		else if (item == 'D')
 		{
 			return DisplacemenT;
 		}
 	}
+	void getPosition() {
+		/*This method updates the position of the plane in the space every time unit.
+		*/
+		int entryTime = Entry_timE;
+		int initialX = X;
+		int initialY = Y;
+		int initialZ = Z;
+		for (int t_clock = 0; t_clock <= 20; t_clock++) {
+			if (entryTime <= t_clock) {
+				//if (0 <= X <= 528000 && 0 <= Y <= 528000 && 15000 <= Z <= 40000) {
+				X = speed_X * (t_clock - entryTime) + initialX;
+				Y = speed_Y * (t_clock - entryTime) + initialY;
+				Z = initialZ;// when the plane enters the space, it maintains its altitude (page 4 of project.pdf)
+				//check if the plans are withing space limit
+				if (((0 <= X) && (X <= 20)) && ((0 <= Y) && (Y <= 20)) && ((0 <= Z) && (Z <= 20))) {
+				cout << endl;
+				cout << "Time (" << t_clock << "): The plan " << ID << " is at (" << X << " , " << Y << " , " << Z<<")";
+				Sleep(500);
+				}
+				}
+			else {
+				Sleep(500);
+				cout << "."; 
+				}
+			
+			
+		}//end clock
+
+	}//end getPosition
+	void collisionDetection(Airplane b) {
+		/*This method checks in advance if the minimum distance is respected; otherwise, it will
+		display a WARNING with the planes concerned, time, and space details
+		*/
+		int entryT = 0;
+		int entryTimeA = Entry_timE;
+		int planeID = ID;
+		int initialXA = X;
+		int initialYA = Y;
+		int initialZA = Z;
+		int entryTimeB = b.Entry_timE;
+		int initialXB = b.X;
+		int initialYB = b.Y;
+		int initialZB = b.Z;
+		int minHorizontalSeparation = 4;
+		int minVerticalSeparation = 3;
+		float distanceH = 20.0;
+		float distanceV = 10.0;
+		
+		if (entryTimeA > entryTimeB)
+			entryT = entryTimeA;
+		else entryT = entryTimeB;
+		Z = initialZA; // when the plane enters the space, it maintains its altitude (page 4 of project.pdf)
+		b.Z = initialZB; 
+		distanceV = abs(Z - b.Z);
+			for (int i = entryT; i <= 20+entryT; i = i++){
+				X = speed_X * (i - entryTimeA) + initialXA;
+				Y = speed_Y * (i - entryTimeA) + initialYA;
+				b.X = b.speed_X * (i - entryTimeB) + initialXB;
+				b.Y = b.speed_Y * (i - entryTimeB) + initialYB;
+				distanceH=horizontalSeperationDistance(X-b.X,Y-b.Y);
+				if (distanceH <= minHorizontalSeparation && distanceV<=minVerticalSeparation) {
+					cout << "WARNING: RISK OF COLLISION!!!\nTime: "<<i<<" between plan "<< planeID<<" at ("<<X<<" , "<<Y<< " , " << Z<<") and plan "<< b.ID<<" at ("<<b.X <<" , "<<b.Y<< " , " << b.Z<<"). \nHorizontal DISTANCE = "<<distanceH<< ". Vertical DISTANCE = "<<distanceV<<endl<<endl;
+					
+				}
+			}//end for i
+			
+	}//end of collisionDetection()
 
 private:
 	int Next_X;
@@ -55,5 +125,9 @@ private:
 	int Next_Z;
 	int DisplacemenT;
 
-
+	float horizontalSeperationDistance(int xCoordinate, int yCoordinate) {
+		return  sqrt( pow(xCoordinate,2) +pow(yCoordinate,2));
+	}
+	
 };
+
--- coen320/src/main.cpp
+++ coen320/src/main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <math.h>
-#include <chrono>
 #include <thread>
 #include "TestCase.h"
 #include "Airplane.h"
 
-#define update_view 5
-
 using namespace std;
 
-//thread 1 for plan view display, update every 5s
-//thread 2 for log file output every 60s
-//thread 3 for safety alarm
-//thread 4 for ATC command
-
-
-void timer_start(function<void(void)> func, unsigned int interval)
-{
-      while (true)
-      {
-          func();
-          this_thread::sleep_for(chrono::milliseconds(interval));
-      }
+void displayPlanPosition (Airplane plane) {
+	
+	plane.getPosition();
 }
-
-void print_plan()
-{
-	cout<<"update view every 5s" <<endl;
+void collisionDet(Airplane a1, Airplane a2){
+	a1.collisionDetection(a2);
 }
-
-void history_log()
-{
+TestCase::TestCase() {
 
 }
 
 int main()
 {
-	timer_start(print_plan,5000);
-	while (true);
+	
+	Airplane P1(0, 1, 0, 0, 0, 10, 10, 0);
+	Airplane P2(1, -1, 0, 0, 20, 12, 10, 25);
+	//thread t0(collisionDet,P1,P2);
+	collisionDet(P1,P2);
+	thread t1(displayPlanPosition,P1);
+	thread t2(displayPlanPosition, P2);
+
+	//t0.join();
+	t1.join();
+	t2.join();
+	
+	
+	/*
+	
+	*/
+	
 }
+
+
