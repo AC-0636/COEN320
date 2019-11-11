5e5a409fbba87d0de326476239c49734905dd766
AC-0636 <33075704+AC-0636@users.noreply.github.com>
2019-11-11 17:14:09 -0500

    bug fixed on input array size

--- coen320/src/main.cpp
+++ coen320/src/main.cpp
@@ -7,9 +7,15 @@
 #define MaxInput 50
 using namespace std;
 
-
+int InputSize;
 TestCase InputArray;
 vector<Airplane> listOfAirplane;
+
+TestCase::TestCase()
+{
+	//get input size from array, but there is not right and will be modified in the later part
+	InputSize = sizeof(InputArray.airplane_schedule);
+}
 
 void dummy2(Airplane plane) {
 	plane.getPosition();
@@ -21,28 +27,32 @@ void dummy2(Airplane plane) {
 
 void getInputFile()
 {
-	
-	/*for (int i = 0; i < sizeof(InputArray.airplane_schedule); i ++)
-	{
-		cout << InputArray.airplane_schedule[i] << " " << endl;
-	}*/
 
-	for (int i = 0; i < sizeof(InputArray.airplane_schedule); i=i+8)
+	for (int i = 0; i < InputSize; i=i+8)
 	{
-		Airplane airplane((InputArray.airplane_schedule[i]),(InputArray.airplane_schedule[i+1]),(InputArray.airplane_schedule[i+2]),
-			(InputArray.airplane_schedule[i+3]),(InputArray.airplane_schedule[i+4]),(InputArray.airplane_schedule[i+5]),(InputArray.airplane_schedule[i+6]),
-			(InputArray.airplane_schedule[i+7]));
-		listOfAirplane.push_back(airplane);
+		if ((InputArray.airplane_schedule[i] == 0) && (i != 0))
+		{
+			//update the actual array size
+			InputSize = i-8;
+			i = 9999999;
+		}
+		else
+		{
+			Airplane airplane((InputArray.airplane_schedule[i]), (InputArray.airplane_schedule[i + 1]), (InputArray.airplane_schedule[i + 2]),
+				(InputArray.airplane_schedule[i + 3]), (InputArray.airplane_schedule[i + 4]), (InputArray.airplane_schedule[i + 5]), (InputArray.airplane_schedule[i + 6]),
+				(InputArray.airplane_schedule[i + 7]));
+			listOfAirplane.push_back(airplane);
+		}
 	}
-
-	
+	//cout << InputSize << endl;
 }
 
 int main()
 {
+	TestCase();
 	getInputFile();
 	thread t1(dummy2,(listOfAirplane[0]));
-	t1.join();
+	//t1.join();
 	//t2.join();
 
 	system("pause");
